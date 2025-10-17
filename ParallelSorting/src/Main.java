bc3e40d60247fcad097dee80c6bce7001d8d0222
gustavo-sen <no-email>
2025-10-17 20:11:03 -0300

    Merge remote-tracking branch 'origin/main'

--- ParallelSorting/src/Main.java
+++ ParallelSorting/src/Main.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
+    static long startTime = 0;
+    static long endTime = 0;
 
     public static void main(String[] args) {
+        int testCase = 0;
 
+        System.out.println("Quick Sort 0");
+        Scanner sc = new Scanner(System.in);
+        testCase = sc.nextInt();
+
+        switch (testCase){
+            case 0:
+
+            default:
+                System.out.println("invalido");
+        }
+    }
+
+    public static void startTimer(){
+        System.out.println("Iniciando Teste");
+        startTime = System.nanoTime();
+    }
+
+    public static void stopTimer(){
+        endTime = System.nanoTime();
+        System.out.println(String.format("Teste Finalizado %d", (startTime - endTime)));
     }
 }
